2e2dbf9a3c9fd14defc6af21e1dbca5df71da65b
majorek31 <m4y0r.offical@gmail.com>
2022-05-02 23:26:18 +0200

    initial release

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="root">
+    <div class="container">
+      <TodoList/>
+    </div>
+    <footer class="footer">
+      <a href="http://github.com/majorek31">Source code</a>
+    </footer>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TodoList from "./components/TodoList.vue"
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TodoList
   }
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body{
+  font-family: Verdana,Geneva,sans-serif; 
+  background-color: #fffaf7;
+}
+.container{
+  width: 800px;
+  margin-left: auto;
+  margin-right: auto;
+}
+.footer {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+a:link{
+  text-decoration: none;
+}
+a:visited{
+  text-decoration: none;
 }
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="todoList">
+        <h4>Your's Todo's:</h4>
+        <div class="todos">
+            <ul v-if="todos.length != 0">
+                <li class="task" v-for="todo in todos" v-bind:key="todo">
+                    {{todo}} <br>
+                </li>
+            </ul>
+            <div v-else style="font-size: 12px">
+                You have no tasks! <br>
+                Add one
+            </div>
+        </div>
+        <form @submit="onSubmit">
+            <input type="text" placeholder="Learn VueJS" v-model="task">
+            <br>
+            <input type="submit" value="Add Task">
+        </form>
+        <span v-if="error">
+            {{error}}
+        </span>
+    </div>
+</template>
+<style scoped>
+.todoList{
+    text-align: center;
+    margin-left: auto;
+    margin-right: auto;
+    width: 60%;
+}
+.todos{
+    width: 300px;
+    margin-left: auto;
+    margin-right: auto;
+}
+.task{
+    text-align: center;
+}
+</style>
+<script>
+export default {
+    data() {
+        return {
+            error: '',
+            task: '',
+            todos: [],
+        }
+    },
+    methods: {
+        mounted(){
+            
+        },
+        onSubmit(event) {
+            event.preventDefault()
+            if (this.task){
+                this.todos.push(this.task)
+                this.error = ''
+                this.task = ''
+            }
+            else
+                this.error = "Your task can't be empty!"
+        }
+    }
+}
+</script>
